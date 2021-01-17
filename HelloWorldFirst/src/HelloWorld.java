73b06a439029df12d6cb4f7940c606974d49cc99
venky <venky@localhost>
2021-01-17 12:55:45 -0600

    This is initial test with the hello World Java Code.

---
+++ HelloWorldFirst/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author venky
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World. Java is working in Eclipse");
+
+	}
+
+}
